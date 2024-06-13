63a0d4037ccfeaa07c5d2e31755708b958d1f434
AlanMartinezDev <alanmartiola@gmail.com>
2024-06-13 16:36:45 +0200

    Instanciar y sincronizar el formulario

--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -1,29 +1,29 @@
 <h1 class="nombre-pagina">Crear cuenta</h1>
 <p class="descripcion-pagina">Llena el siguiente formulario para crear una cuenta</p>
 
-<form action="/" class="formulario" method="post">
+<form action="" class="formulario" method="post">
     <div class="campo">
         <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre" placeholder="Tu nombre">
+        <input type="text" name="nombre" id="nombre" placeholder="Tu nombre" value="<?php echo s($usuario->nombre); ?>">
     </div>
     <div class="campo">
         <label for="apellido">Apellido</label>
-        <input type="text" name="apellido" id="apellido" placeholder="Tu apellido">
+        <input type="text" name="apellido" id="apellido" placeholder="Tu apellido" value="<?php echo s($usuario->apellido); ?>">
     </div>
     <div class="campo">
         <label for="telefono">Teléfono</label>
-        <input type="tel" name="telefono" id="telefono" placeholder="Tu teléfono">
+        <input type="tel" name="telefono" id="telefono" placeholder="Tu teléfono" value="<?php echo s($usuario->telefono); ?>">
     </div>
     <div class="campo">
         <label for="email">Email</label>
-        <input type="email" name="email" id="email" placeholder="Tu email">
+        <input type="email" name="email" id="email" placeholder="Tu email" value="<?php echo s($usuario->email); ?>">
     </div>
     <div class="campo">
         <label for="password">Contraseña</label>
         <input type="password" name="password" id="password" placeholder="Tu contraseña">
     </div>
 
-    <input class="boton" type="submit" value="Crear cuenta" c>
+    <input class="boton" type="submit" value="Crear cuenta">
 </form>
 
 <div class="acciones">
