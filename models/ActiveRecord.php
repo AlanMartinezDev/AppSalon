293b14cb4ad64369646ebe46ebb43469bc810bc0
AlanMartinezDev <alanmartiola@gmail.com>
2024-06-26 16:53:34 +0200

    Añadir método where

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -131,6 +131,14 @@ class ActiveRecord
         return array_shift($resultado);
     }
 
+    // Busca un registro por su id
+    public static function where($columna, $valor)
+    {
+        $query = "SELECT * FROM " . static::$tabla  . " WHERE {$columna} = '{$valor}'";
+        $resultado = self::consultarSQL($query);
+        return array_shift($resultado);
+    }
+
     // Obtener Registros con cierta cantidad
     public static function get($limite)
     {
