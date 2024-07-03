cb8e3a198667c71efcf923a9d5e673f9f0f74206
AlanMartinezDev <alanmartiola@gmail.com>
2024-07-03 16:40:04 +0200

    Login con verificación de password y token

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -10,7 +10,41 @@ class LoginController
 {
     public static function login(Router $router)
     {
-        $router->render('auth/login', []);
+        $alertas = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $auth = new Usuario($_POST);
+            $alertas = $auth->validarLogin();
+
+            if (empty($alertas)) {
+                // Comprobar que exista el usuario
+                $usuario = Usuario::where('email', $auth->email);
+
+                if ($usuario) {
+                    // Verificar la contraseña
+                    if ($usuario->comprobarPasswordAndVerificado($auth->password)) {
+                        // Autenticar el usuario
+                        session_start();
+
+                        $_SESSION['id'] = $usuario->id;
+                        $_SESSION['nombre'] = $usuario->nombre . " " . $usuario->apellido;
+                        $_SESSION['email'] = $usuario->email;
+                        $_SESSION['login'] = true;
+
+                        // Redireccionamiento
+                        
+                    }
+                } else {
+                    Usuario::setAlerta('error', 'Usuario no encontrado');
+                }
+            }
+        }
+
+        $alertas = Usuario::getAlertas();
+
+        $router->render('auth/login', [
+            'alertas' => $alertas
+        ]);
     }
 
     public static function logout()
