89ec8bb9aaba5c2c9471512fe6dbadaa2e3855d1
AlanMartinezDev <alanmartiola@gmail.com>
2024-07-19 16:33:21 +0200

    Formulario de recuperar contraseña

---
+++ views/auth/recuperar-password.php
@@ -0,0 +1,16 @@
+<h1 class="nombre-pagina">Recuperar contraseña</h1>
+<p class="descripcion-pagina">Coloca tu nueva contraseña a continuación</p>
+
+<form method="post" class="formulario">
+    <div class="campo">
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" placeholder="Tu nueva contraseña">
+    </div>
+
+    <input type="submit" class="boton" value="Guardar">
+</form>
+
+<div class="acciones">
+    <a href="/">¿Ya tienes cuenta? Iniciar sesión</a>
+    <a href="/crear-cuenta">Crear cuenta</a>
+</div>
