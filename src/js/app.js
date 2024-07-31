3cceed1fa649fe087bd6e7068c72f90a02cad6bc
AlanMartinezDev <alanmartiola@gmail.com>
2024-07-31 16:53:06 +0200

    Finalizar la navegación por tabs

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function iniciarApp() {
+    mostrarSeccion(); // Muestra y oculta las secciones
     tabs(); // Cambia la sección cuando se presionen los tabs
 }
 
@@ -19,11 +20,20 @@ function mostrarSeccion() {
     const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add('mostrar');
+
+    // Quita la clase de actual al tab anterior
+    const tabAnterior = document.querySelector('.actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+
+    // Resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
 
 function tabs() {
     const botones = document.querySelectorAll('.tabs button');
-
     botones.forEach((boton) => {
         boton.addEventListener('click', function (e) {
             paso = parseInt(e.target.dataset.paso);
