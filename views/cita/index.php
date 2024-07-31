d7dadfcc88c79d1434df669367bfbecff42e9c6d
AlanMartinezDev <alanmartiola@gmail.com>
2024-07-31 16:26:35 +0200

    Tabs y botones para la navegación

--- views/cita/index.php
+++ views/cita/index.php
@@ -2,15 +2,21 @@
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
 <div id="app">
+    <nav class="tabs">
+        <button class="actual" type="button" data-paso="1">Servicios</button>
+        <button type="button" data-paso="2">Información cita</button>
+        <button type="button" data-paso="3">Resumen</button>
+    </nav>
+
     <div class="seccion" id="paso-1">
         <h2>Servicios</h2>
-        <p>Elige tus servicios a continuación</p>
+        <p class="text-center">Elige tus servicios a continuación</p>
         <div class="listado-servicios" id="servicios"></div>
     </div>
 
     <div class="seccion" id="paso-2">
         <h2>Tus datos y cita</h2>
-        <p>Coloca tus datos y fecha de tu cita</p>
+        <p class="text-center">Coloca tus datos y fecha de tu cita</p>
 
         <form class="formulario">
             <div class="campo">
@@ -32,6 +38,11 @@
 
     <div class="seccion" id="paso-3">
         <h2>Resumen</h2>
-        <p>Verifica que la información sea correcta</p>
+        <p class="text-center">Verifica que la información sea correcta</p>
+    </div>
+
+    <div class="paginacion">
+        <button class="boton" id="anterior">&laquo; Anterior</button>
+        <button class="boton" id="anterior">Siguiente &raquo;</button>
     </div>
 </div>
