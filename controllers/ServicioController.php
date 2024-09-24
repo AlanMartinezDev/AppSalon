1d680f70e3ebe9f2ce3ed1cd841eb4342731c668
AlanMartinezDev <alanmartiola@gmail.com>
2024-09-24 15:54:48 +0200

    Llenar el render con la variable de sesión

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -8,19 +8,37 @@ class ServicioController
 {
     public static function index(Router $router)
     {
-        $router->render('servicios/index', []);
+        if (!$_SESSION['nombre']) {
+            session_start();
+        }
+
+        $router->render('servicios/index', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function crear(Router $router)
     {
+        if (!$_SESSION['nombre']) {
+            session_start();
+        }
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
+
+        $router->render('servicios/crear', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function actualizar(Router $router)
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
+
+        $router->render('servicios/actualizar', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function eliminar(Router $router) {}
