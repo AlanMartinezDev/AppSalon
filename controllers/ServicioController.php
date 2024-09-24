b225d823996c5bbd3bb1937e5fd0ae0c356325bc
AlanMartinezDev <alanmartiola@gmail.com>
2024-09-24 16:11:22 +0200

    Validar y crear los servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Servicio;
 use MVC\Router;
 
 class ServicioController
@@ -23,11 +24,23 @@ class ServicioController
             session_start();
         }
 
+        $servicio = new Servicio;
+        $alertas = [];
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $servicio->sincronizar($_POST);
+            $alertas = $servicio->validar();
+
+            if (empty($alertas)) {
+                $servicio->guardar();
+                header('Location: /servicios');
+            }
         }
 
         $router->render('servicios/crear', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'servicio' => $servicio,
+            'alertas' => $alertas
         ]);
     }
 
